64c85122707ec7adf31548aed5aafe0c446cd4d8
parkjoonsoo0419 <parkjoonsoo00@gmail.com>
2025-11-01 16:43:02 +0900

    Update Module 1-2.java

--- Module 1-2.java
+++ Module 1-2.java
@@ -1,7 +1,8 @@
-public class HelloCycle {
+public class Progeamm_development {
     public static void main(String[] args) {
         System.out.println("Step 1: Edit");
         System.out.println("Step 2: Compile");
         System.out.println("Step 3: Run");
     }
 }
+
